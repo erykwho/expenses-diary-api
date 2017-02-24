3172374c95050d03e86ffdfad03411c59ebcd09c
Eryk Humberto Oliveira Alves <erykhumberto@hotmail.com>
2017-02-24 20:31:21 -0300

    Implement use of logger

--- run.py
+++ run.py
@@ -1,9 +1,15 @@
 from project import app
 from config.config import Config
+from logger.logger import new
+
+logger = new("Run")
 
 if __name__ == '__main__':
     app.config.from_object(Config())
+    host = '0.0.0.0'
+    port = app.config.get("PORT")
+    logger.info("Iniciando aplicacao no host {} e porta {}".format(host, port))
     app.run(
-        host='0.0.0.0',
-        port=app.config.get("PORT")
+        host=host,
+        port=port
     )
