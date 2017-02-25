647be8143caed87ae40a79c3a0ef114985c8913c
Eryk Humberto Oliveira Alves <erykhumberto@hotmail.com>
2017-02-25 01:01:39 -0300

    Alter the location of cursor methods

--- project/expense/expense.py
+++ project/expense/expense.py
@@ -1,30 +1,10 @@
 import flask_restful as restful
-from psycopg2.extras import RealDictCursor
 
 from database.connection import db_conn
+from database.execute import execute_to_json, execute_to_scalar
 from logger.logger import new
 
 logger = new("Expense")
-
-
-def execute_to_json(conn, query):
-    with conn.cursor(cursor_factory=RealDictCursor) as cursor:
-        cursor.execute(query)
-
-        results = []
-        for row in cursor.fetchall():
-            results.append(dict(zip(row.keys(), row.values())))
-
-    return results
-
-
-def execute_to_scalar(conn, query):
-    with conn.cursor() as cursor:
-        cursor.execute(query)
-
-        result = cursor.fetchone()
-
-    return result[0]
 
 
 class Expense(restful.Resource):
@@ -35,38 +15,42 @@ class Expense(restful.Resource):
     def get():
         logger.info("GET - Expense")
 
-        conn = db_conn()
+        try:
+            conn = db_conn()
 
-        query_count = '''
-        SELECT
-            COUNT(*)
-        FROM
-            "expense"
-        WHERE
-            is_active IS true;
-        '''
+            query_count = '''
+            SELECT
+                COUNT(*)
+            FROM
+                "expense"
+            WHERE
+                is_active IS true;
+            '''
 
-        query_select = '''
-        SELECT
-            user_id,
-            payment_origin_id,
-            category_id,
-            reference_date,
-            description,
-            amount,
-            regreted,
-            comments
-        FROM
-            "expense"
-        WHERE is_active IS true;
-        '''
+            query_select = '''
+            SELECT
+                user_id,
+                payment_origin_id,
+                category_id,
+                reference_date,
+                description,
+                amount,
+                regreted,
+                comments
+            FROM
+                "expense"
+            WHERE is_active IS true;
+            '''
 
-        result = execute_to_json(conn, query_select)
-        count = execute_to_scalar(conn, query_count)
+            result = execute_to_json(conn, query_select)
+            count = execute_to_scalar(conn, query_count)
 
-        response = dict()
-        response['content'] = result
-        response['total'] = count
+            response = dict()
+            response['content'] = result
+            response['total'] = count
 
-        conn.close()
-        return response, 200
+            conn.close()
+            return response, 200
+        except Exception as error:
+            logger(error)
+            return error, 500
