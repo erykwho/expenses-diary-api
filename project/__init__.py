e6679f70aad7b42edad99fba0f2a04a84d1ba338
Eryk Humberto Oliveira Alves <erykhumberto@hotmail.com>
2017-03-12 22:58:57 -0300

    Add resource Category and methods GET, POST, PATCH and DELETE

--- project/__init__.py
+++ project/__init__.py
@@ -1,6 +1,8 @@
 from flask import Flask
 from project.expense import expense
+from project.category import category
 
 app = Flask(__name__)
 
 app.register_blueprint(expense)
+app.register_blueprint(category)
