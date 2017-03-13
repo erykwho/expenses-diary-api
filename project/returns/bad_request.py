553739977e3773ecf00b03848c471e8a0466e462
Eryk Humberto Oliveira Alves <erykhumberto@hotmail.com>
2017-03-12 23:29:20 -0300

    Adiciona ponto final no final dos retornos padroes

--- project/returns/bad_request.py
+++ project/returns/bad_request.py
@@ -3,11 +3,11 @@ from project.returns import HTTP_BAD_REQUEST
 
 def must_be_integer():
     return {
-               "message": "Valor inserido deve ser um inteiro"
+               "message": "Valor inserido deve ser um inteiro."
            }, HTTP_BAD_REQUEST
 
 
 def missing_fields(*args):
     return {
-               "message": "Os campos (%s) devem ser enviados" % ', '.join(str(arg) for arg in args)
+               "message": "Os campos (%s) devem ser enviados." % ', '.join(str(arg) for arg in args)
            }, HTTP_BAD_REQUEST
--- project/returns/internal_server_error.py
+++ project/returns/internal_server_error.py
@@ -3,5 +3,5 @@ from project.returns import HTTP_INTERNAL_SERVER_ERROR
 
 def unexpected_error():
     return {
-               "message": "Um erro inesperado ocorreu"
+               "message": "Um erro inesperado ocorreu."
            }, HTTP_INTERNAL_SERVER_ERROR
