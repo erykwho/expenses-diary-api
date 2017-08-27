05e1f209554e1d2593db8f76133a3405d2a1b76a
Eryk Humberto Oliveira Alves <erykhumberto@hotmail.com>
2017-08-27 18:03:04 -0300

    Integrate authentication with expenses endpoint

---
+++ authentication/authentication.py
@@ -0,0 +1,17 @@
+from functools import wraps
+
+from flask import request, abort
+
+from exceptions.authentication_failed import AuthenticationFailed
+
+
+def login_required(f):
+    @wraps(f)
+    def wrap(*args, **kwargs):
+        user_id = request.headers.get('User-Id')
+        if user_id:
+            return f(*args, **kwargs)
+        else:
+            return abort(401)
+
+    return wrap
