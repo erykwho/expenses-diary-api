8c0d649ce0dc9cee21fbcf4ca382b98a2ebe464a
Eryk Humberto Oliveira Alves <erykwho@gmail.com>
2017-11-21 10:57:44 -0200

    Implement camelCase return and wrapper for case conversion (camelCase to snakeCase)

--- tests/utils/test_convert_string.py
+++ tests/utils/test_convert_string.py
@@ -11,5 +11,79 @@ class TestCamelCaseToSnakeCase(unittest.TestCase):
         """
         value = 'fooBar'
         expected = 'foo_bar'
-        actual = StringConverter.camel_case_to_snake_case(value)
+        actual = StringConverter().camel_case_to_snake_case(value)
+        self.assertEqual(expected, actual)
+
+    def test_camel_case_to_snake_case_list(self):
+        """
+            Asserts if converts all items of a list
+            from camelCase to snakeCase correctly
+        """
+        items = [
+            'fooBar',
+            'pipTchu',
+            'erykWho'
+        ]
+        expected = [
+            'foo_bar',
+            'pip_tchu',
+            'eryk_who'
+        ]
+        actual = StringConverter().camel_case_to_snake_case(items)
+        self.assertEqual(expected, actual)
+
+    def test_camel_case_to_snake_case_dict(self):
+        """
+            Asserts if converts all keys of a dict
+            from camelCase to snakeCase correctly
+        """
+        my_dict = {
+            'fooBar': 'a',
+            'pipTchu': 'b',
+            'erykWho': 'c'
+        }
+        expected = {
+            'foo_bar': 'a',
+            'pip_tchu': 'b',
+            'eryk_who': 'c'
+        }
+        actual = StringConverter().camel_case_to_snake_case(my_dict)
+        self.assertEqual(expected, actual)
+
+    def test_snake_case_to_camel_case(self):
+        """
+            Asserts if converts a snakeCase name to camelCase
+            correctly
+        """
+        value = 'foo_bar'
+        expected = 'fooBar'
+        actual = StringConverter().snake_case_to_camel_case(value)
+        self.assertEqual(expected, actual)
+
+    def test_snake_case_to_camel_case_list(self):
+        """
+            Asserts if converts all the items of a list
+            from snakeCase to camelCase correctly
+        """
+        items = ['foo_bar', 'pip_tchu', 'eryk_who']
+        expected = ['fooBar', 'pipTchu', 'erykWho']
+        actual = StringConverter().snake_case_to_camel_case(items)
+        self.assertEqual(expected, actual)
+
+    def test_snake_case_to_camel_case_dict(self):
+        """
+            Asserts if converts all the keys from a dict
+            from snakeCase to camelCase correctly
+        """
+        my_dict = {
+            'foo_bar': 'a',
+            'pip_tchu': 'b',
+            'eryk_who': 'c'
+        }
+        expected = {
+            'fooBar': 'a',
+            'pipTchu': 'b',
+            'erykWho': 'c'
+        }
+        actual = StringConverter().snake_case_to_camel_case(my_dict)
         self.assertEqual(expected, actual)
--- utils/convert_string.py
+++ utils/convert_string.py
@@ -1,8 +1,57 @@
 import re
+from functools import wraps
+
+
+def camel_case_to_snake_case_wrapper(f):
+    @wraps(f)
+    def wrap(*args, **kwargs):
+        new_args = (StringConverter().camel_case_to_snake_case(arg) for arg in args)
+        return f(*new_args, **kwargs)
+
+    return wrap
 
 
 class StringConverter:
+    def camel_case_to_snake_case(self, items):
+        if isinstance(items, dict):
+            new_items = {}
+            for key, value in items.items():
+                new_key = self._camel_case_to_snake_case(key)
+                new_items[new_key] = value
+
+            return new_items
+
+        if isinstance(items, list):
+            for i, item in enumerate(items):
+                items[i] = self._camel_case_to_snake_case(item)
+
+            return items
+
+        return self._camel_case_to_snake_case(items)
+
     @staticmethod
-    def camel_case_to_snake_case(value):
-        s1 = re.sub('(.)([A-Z][a-z]+)', r'\1_\2', value)
+    def _camel_case_to_snake_case(item):
+        s1 = re.sub('(.)([A-Z][a-z]+)', r'\1_\2', item)
         return re.sub('([a-z0-9])([A-Z])', r'\1_\2', s1).lower()
+
+    def snake_case_to_camel_case(self, items):
+        if isinstance(items, dict):
+            new_items = {}
+            for key, value in items.items():
+                new_key = self._snake_case_to_camel_case(key)
+                new_items[new_key] = value
+            return new_items
+
+        if isinstance(items, list):
+            for i, item in enumerate(items):
+                items[i] = self._snake_case_to_camel_case(item)
+            return items
+
+        return self._snake_case_to_camel_case(items)
+
+    @staticmethod
+    def _snake_case_to_camel_case(item):
+        components = item.split('_')
+        # We capitalize the first letter of each component except the first one
+        # with the 'title' method and join them together.
+        return components[0] + "".join(x.title() for x in components[1:])
